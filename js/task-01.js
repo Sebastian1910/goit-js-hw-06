2862f2f2dcea5891525eaa282464f32280826e72
Sebastian1910 <150168659+Sebastian1910@users.noreply.github.com>
2024-03-19 21:07:03 +0100

    aa

---
+++ js/task-01.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const categoriesList = document.querySelectorAll("#categories .item");
+
+  console.log("Liczba kategorii:", categoriesList.length);
+
+  categoriesList.forEach(function (category) {
+    const categoryName = category.querySelector("h2").textContent;
+    const itemCount = category.querySelectorAll("ul li").length;
+    console.log(`Kategoria: ${categoryName}, Liczba elementów: ${itemCount}`);
+  });
+});
